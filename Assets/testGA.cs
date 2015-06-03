e5bf0e0e639ca37b53ca90f25ef5cbb7fd79800c
Restia <akahi.void@gmail.com>
2015-06-03 16:58:52 +0700

    google analytics imported

---
+++ Assets/testGA.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testGA : MonoBehaviour {
+
+    public GoogleAnalyticsV3 analytic;
+
+	// Use this for itialization
+	void Start () {
+        analytic.StartSession();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("test");
+            analytic.LogScreen("test");
+        }
+	}
+}
